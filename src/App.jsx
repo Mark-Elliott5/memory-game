2237d61be24fe7cdefecbfae0e863f069333db53
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2023-10-24 04:17:30 -0400

    Style/layout changes, App/main reorganization

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import GameContextProvider from './context/GameContextProvider';
 import TileWrapper from './TileWrapper';
 import Scoreboard from './Scoreboard';
+import githubMark from './assets/github-mark-white.png';
 
 function App() {
   return (
     <>
       <GameContextProvider>
-        <Scoreboard />
+        <header>
+          <img src="cartoonnetworklogo.png" />
+          <p>Memory Game</p>
+          <Scoreboard />
+        </header>
         <TileWrapper />
       </GameContextProvider>
+      <footer>
+        Designed by Mark Elliott{' '}
+        <a href="https://github.com/Mark-Elliott5">
+          <img className="github-link" src={githubMark} />
+        </a>
+      </footer>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import githubMark from './assets/github-mark-white.png';
 import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <header>
-      <img src="cartoonnetworklogo.png" />
-      <p>Memory Game</p>
-    </header>
     <App />
-    <footer>
-      Designed by Mark Elliott{' '}
-      <a href="https://github.com/Mark-Elliott5">
-        <img className="github-link" src={githubMark} />
-      </a>
-    </footer>
   </React.StrictMode>,
 );
